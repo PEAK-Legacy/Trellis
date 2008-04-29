5a5e84d14b4f4368257fe740d73c2aca1f2e15a0
PJ Eby <pje@telecommunity.com>
2008-04-29 00:25:17 +0000

    Bump version after release
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/Trellis@2530 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 # Metadata
 PACKAGE_NAME = "Trellis"
-PACKAGE_VERSION = "0.7a1"
+PACKAGE_VERSION = "0.7a2"
 PACKAGES = ['peak', 'peak.events']
 def get_description():
     # Get our long description from the documentation
